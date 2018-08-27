196eaab0ef751a88a8562f21d9929a95bb72ed01
jesse walsh <jessewalsh@jesses-MacBook-Pro.local>
2018-08-27 15:30:08 +1200

    first push

--- oldCode/customCode/js/custom.js
+++
@@ -1,7 +0,0 @@
-// iife to define scope of code
-(function () {
-    $(document).ready(function() {
-        console.log('A');
-    });
-//iife ends
-})();
--- oldCode/notworkingrefactor/includes/widget.php
+++
@@ -1,4 +0,0 @@
-<div class="well">
-    <h4>Side Widget Well</h4>
-    <p>lhygouygoyufgoytf, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-</div>
